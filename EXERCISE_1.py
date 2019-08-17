ada06621d8a6a9116cee489e40b1bde295f2cc0c
miloszfoksinski <miloszfoksinski@gmail.com>
2019-08-17 20:05:49 +0200

    update

--- EXERCISE_1.py
+++ EXERCISE_1.py
@@ -1,3 +1,11 @@
-print('aaa')
-print('bbb')
-print('ccc')
+import datetime
+
+
+name = input('Write Your name: ')
+age = int(input('Write how old are You: '))
+
+today = datetime.datetime.now()
+today_year = today.year
+age_100 = today_year + (100-age)
+
+print(name,', You will be 100 years old in ',age_100)
